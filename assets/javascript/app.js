31c087bd7c06faae842abe768d69e5cc8752b17b
Brock Cowen <brock12cowen@gmail.com>
2019-04-18 20:31:48 -0500

    Setup pages

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,7 +2,8 @@ var date = moment().format("YYYY-MM-DD");
 var end = moment().add(14, 'days').format("YYYY-MM-DD");
 console.log(end);
 var count = 0;
-
+var page = 0;
+var miles;
 
 
 
@@ -12,11 +13,38 @@ $("#add-params").on("click", function(){
 
 city = $("#city-input").val();
 localStorage.setItem('city', city);
-var miles = $("#miles-input").val();
+miles = $("#miles-input").val();
 state = $("#select-state option:selected").attr("value");
 localStorage.setItem('state', state);
 console.log(state);
-var queryURL = `https://app.ticketmaster.com/discovery/v2/events.json?city=${city}&stateCode=${state}&startDateTime=${date}T14:00:00Z&endDateTime=${end}T14:00:00Z&radius=${miles}&unit=miles&apikey=VVhqdJgL8bOLqDeCOvQzEaDiHBKw5xvC`;
+var queryURL = `https://app.ticketmaster.com/discovery/v2/events.json?city=${city}&stateCode=${state}&startDateTime=${date}T14:00:00Z&endDateTime=${end}T14:00:00Z&radius=${miles}&unit=miles&size=10&page=${page}&apikey=VVhqdJgL8bOLqDeCOvQzEaDiHBKw5xvC`;
+console.log(queryURL);
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+      console.log(response);
+    $("#instructions").html(`<p>Browse the list of events over the next 2 weeks below and click the "Select" button next to your choice.</p>
+    <p>You can click the "Link to Details" to view in TicketMaster more details on it.</p>`);
+    $("#options").append(response._embedded.events.map(show));
+    $("#next").html(makePages(response.page.totalPages));
+    count = 0;
+  });
+});
+
+function makePages(p){
+  var pages = [];
+  for(var i = 0; i<p; i++){
+    pages.push(`<span class="page" data-page="${i}">${i+1}</span>`)
+  }
+  return pages.join();
+}
+
+$(document).on("click", ".page", function(){
+page = $(this).attr("data-page");
+console.log(this);
+$("#options").html("");
+var queryURL = `https://app.ticketmaster.com/discovery/v2/events.json?city=${city}&stateCode=${state}&startDateTime=${date}T14:00:00Z&endDateTime=${end}T14:00:00Z&radius=${miles}&unit=miles&size=10&page=${page}&apikey=VVhqdJgL8bOLqDeCOvQzEaDiHBKw5xvC`;
 console.log(queryURL);
 $.ajax({
     url: queryURL,
@@ -29,7 +57,6 @@ $.ajax({
     count = 0;
   });
 });
-
 
 $(document).on("click", ".select", function(){
     localStorage.setItem('event', $(this).attr("event"));
