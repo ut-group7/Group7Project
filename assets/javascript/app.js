a310f7116f11e6e9f4c4184f8f6363cabaea4e76
Gregory Rutherford <gregorford@gmail.com>
2019-04-20 11:19:53 -0500

    updated radio buttons to checkboxes, added purchase link at the end page and hid the pages on the end page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -62,6 +62,8 @@ $(document).on("click", ".select", function(){
     localStorage.setItem('event', $(this).attr("event"));
     localStorage.setItem('time', $(this).attr("time"));
     localStorage.setItem('date', $(this).attr("date"));
+    localStorage.setItem('link', $(this).attr("link"));
+    $("#next").html(``);
     $("#options").html(``);
     $("#instructions").html(``);
     $("#find").html(`
@@ -72,10 +74,10 @@ $(document).on("click", ".select", function(){
                 </div>
             </th>
             <th>
-            $: <input type="radio" name="price" class="myCheck"  value="1">
-            $$: <input type="radio" name="price2" class="myCheck"  value="2">
-            $$$: <input type="radio" name="price3" class="myCheck"  value="3">
-            $$$$: <input type="radio" name="price4" class="myCheck"  value="4">
+            $: <input type="checkbox" name="price" class="myCheck"  value="1">
+            $$: <input type="checkbox" name="price2" class="myCheck"  value="2">
+            $$$: <input type="checkbox" name="price3" class="myCheck"  value="3">
+            $$$$: <input type="checkbox" name="price4" class="myCheck"  value="4">
             </th>
             <br>
             <th>
@@ -90,9 +92,9 @@ function show(r){
     var i = count;
     var time = moment(r.dates.start.localTime, 'HH:mm').format('hh:mm a');
     count ++;
-    return `</br><button class="select" value="${i}" event="${r.name}" time="${time}" date="${r.dates.start.localDate}">select</button>
+    return `</br><button class="select" value="${i}" event="${r.name}" time="${time}" date="${r.dates.start.localDate}" link="${r.url}">select</button>
     <h5>${r.name}</h5><p>Date/Time: ${r.dates.start.localDate} ${time}</p>
-    <a href="${r.url}">Link to Details</a>
+    <a href="${r.url}" target="_blank">Link to Details</a>
     <img src="${r.images[0].url}"/></br>
     <hr>`
 }
@@ -106,7 +108,6 @@ $(document).on("click", "#new-params", function(){
     var dollars3 = $("input[name='price3']:checked").val();
     var dollars4 = $("input[name='price4']:checked").val();
     var dollarSign = isChecked(dollars, dollars2, dollars3, dollars4);
-    console.log(dollars, dollars2, dollars3, dollars4);
 
     function isChecked (dollars, dollars2, dollars3, dollars4) {
     var dollarSign = "";
@@ -160,7 +161,7 @@ function display(r){
     count ++;
     return `</br><button class="newSelect" value="${i}" name="${r.name}">select</button>
     <h5>${r.name}</h5><p>Price: ${r.price} Phone Number: ${r.display_phone} Address: ${r.location.display_address[0]}, ${r.location.display_address[1]}</p>
-    <a href="${r.url}">Link to Details on Yelp</a>
+    <a href="${r.url}" target="_blank">Link to Details on Yelp</a>
     <img src="${r.image_url}"/></br>
     <hr>`
 }
@@ -172,7 +173,8 @@ $(document).on("click", ".newSelect", function(){
     $("#find").html(``);
     $("#choices").html(``);
     $("#results").html(`
-    <h1>You are going to eat at ${$(this).attr("name")}.<h1>
-    <h1>And then going to ${localStorage.getItem('event')} at ${localStorage.getItem('time')} on ${localStorage.getItem('date')}
+    <h1>You are going to eat at ${$(this).attr("name")}.</h1>
+    <h1>And then going to ${localStorage.getItem('event')} at ${localStorage.getItem('time')} on ${localStorage.getItem('date')}</h1>
+    <a href="${localStorage.getItem('link')}" target="_blank">Purchase Tickets</a>
     `);
 });
